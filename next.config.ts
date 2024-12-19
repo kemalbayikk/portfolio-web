ff65333e9328221113c3487cc8b9473f77421cff
Kemal Bayık <45203337+kemalbayikk@users.noreply.github.com>
2024-12-19 03:27:38 +0000

    latest build paths removed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio-web', // Repo adınızı buraya yazın
-  assetPrefix: '/portfolio-web', // Statik dosyalar için doğru yolu ayarlayın
-  trailingSlash: true, // Tüm URL'lerin "/" ile bitmesini sağlar
+  output: 'export', // Statik export modu
+  trailingSlash: true, // URL'lerin sonunda '/' bulunmasını sağlar
+  // basePath ve assetPrefix ayarlarını yerel çalışmada devre dışı bırakıyoruz
+  // basePath: process.env.NODE_ENV === 'production' ? '/portfolio-web' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-web' : '',
 };
 
 module.exports = nextConfig;
