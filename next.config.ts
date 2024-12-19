e6fbab301721a053000c39bb8cf90dddfa4ea7fc
Kemal Bayık <45203337+kemalbayikk@users.noreply.github.com>
2024-12-19 15:39:34 +0000

    nojekyll added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   basePath: '/portfolio-web', // GitHub repo adıyla aynı
-  assetPrefix: '/portfolio-web', // Statik dosyalar için prefix
+  assetPrefix: '/portfolio-web/', // Statik dosyalar için prefix
 };
 
 module.exports = nextConfig;
