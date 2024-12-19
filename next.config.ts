553071d6fbe8afeb8c5dead4fc34daa118db436e
Kemal Bayık <45203337+kemalbayikk@users.noreply.github.com>
2024-12-19 03:02:59 +0000

    new build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: './', // GitHub Pages için gerekli
+  basePath: '/portfolio-website', // Repo adınızı buraya yazın
+  assetPrefix: '/portfolio-website', // Statik dosyalar için doğru yolu ayarlayın
+  trailingSlash: true, // Tüm URL'lerin "/" ile bitmesini sağlar
 };
 
 module.exports = nextConfig;
